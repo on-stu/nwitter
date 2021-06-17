0d6ca63373aa39b70f31b494d0e9e3456038fe99
onstu <minsu0523@naver.com>
2021-06-17 17:59:09 +0900

    net

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFqBFUERyG86duSFH6QgiTdn1LW4U-ZjA",
+    authDomain: "fir-practice-55cc5.firebaseapp.com",
+    projectId: "fir-practice-55cc5",
+    storageBucket: "fir-practice-55cc5.appspot.com",
+    messagingSenderId: "253577554696",
+    appId: "1:253577554696:web:821ba5393de6503d22dabd",
+    measurementId: "G-2JT7BLZHQK"
+  };
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
